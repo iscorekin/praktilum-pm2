c97ce6a6764e04bcb82885b48a8c392d26ade305
Nikita Iskorkin <iscorekin@gmail.com>
2024-12-09 02:41:44 +0300

    Fixed ref

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -12,7 +12,7 @@ module.exports = {
       key: '../praktikum-ubuntu.pem',
       user: process.env.REMOTE_USER,
       host: process.env.REMOTE_HOST,
-      ref: 'origin/master',
+      ref: process.env.REF,
       repo: process.env.REPO,
       path: process.env.REMOTE_PATH,
       'post-deploy': `cd backend && npm install && scp .env ${process.env.REMOTE_USER}@${process.env.REMOTE_HOST}:${process.env.REMOTE_PATH} && pm2 startOrRestart ecosystem.config.js --env production`,
