345a8ff3a2ce77eb67c1f09caf981aef57912dfa
Nikita Iskorkin <iscorekin@gmail.com>
2024-12-09 02:43:21 +0300

    fix

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -15,7 +15,7 @@ module.exports = {
       ref: process.env.REF,
       repo: process.env.REPO,
       path: process.env.REMOTE_PATH,
-      'post-deploy': `cd backend && npm install && scp .env ${process.env.REMOTE_USER}@${process.env.REMOTE_HOST}:${process.env.REMOTE_PATH} && pm2 startOrRestart ecosystem.config.js --env production`,
+      'post-deploy': `cd backend && npm install && scp ./.env ${process.env.REMOTE_USER}@${process.env.REMOTE_HOST}:${process.env.REMOTE_PATH} && pm2 startOrRestart ecosystem.config.js --env production`,
     },
   },
 };
